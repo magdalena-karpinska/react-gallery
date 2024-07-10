08627259e61f8813b02fd04d6100545279331bb5
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-07-10 17:07:30 +0200

    Added pagination, buttons to prev/next page
    Co-authored-by: Kenny Dinh <kenny.t.dinh93@gmail.com>
    Co-authored-by: Robin Sahin <robin.sahin88@gmail.com>
    Co-authored-by: Magdalena Karpinska <magdalena.karpinska13@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import './App.scss';
-import { FormEvent, useState } from 'react';
-import { Form } from './components/form/Form';
-import { Navbar } from './components/navbar/Navbar';
-import { getImages } from './services/getImages';
-import { Image } from './models/Image';
-import { DisplayImages } from './components/displayImages/DisplayImages';
+import "./App.scss";
+import { FormEvent, useState } from "react";
+import { Form } from "./components/form/Form";
+import { Navbar } from "./components/navbar/Navbar";
+import { getImages } from "./services/getImages";
+import { Image } from "./models/Image";
+import { DisplayImages } from "./components/displayImages/DisplayImages";
 
 function App() {
-  const [searchWord, setSearchWord] = useState('');
+  const [searchWord, setSearchWord] = useState("");
   const [imageData, setImageData] = useState<Image[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    const data: Image[] = await getImages(searchWord);
-    console.log(data);
+    const data = await getImages(searchWord, 1);
     setImageData(data);
+    setCurrentPage(1);
+  };
+
+  const handlePageChange = async (pageNumber: number) => {
+    const data = await getImages(searchWord, pageNumber);
+    setImageData(data);
+    setCurrentPage(pageNumber);
   };
 
   return (
@@ -26,6 +33,15 @@ function App() {
         searchWord={searchWord}
       />
       <DisplayImages images={imageData} />
+      <div>
+        {currentPage > 1 && (
+          <button onClick={() => handlePageChange(currentPage - 1)}>
+            Previous
+          </button>
+        )}
+
+        <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
+      </div>
     </>
   );
 }
--- src/services/getImages.ts
+++ src/services/getImages.ts
@@ -1,9 +1,9 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 
-export const getImages = async (searchWord: string) => {
+export const getImages = async (searchWord: string, page: number = 1) => {
   try {
     const res = await fetch(
-      `https://api.unsplash.com/search/photos?query=${searchWord}&client_id=${apiKey}`
+      `https://api.unsplash.com/search/photos?query=${searchWord}&client_id=${apiKey}&page=${page}`
     );
     const data = await res.json();
     return data.results;
