82a95b1206266f7c7d39d9b6dc842ae8af535673
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-07-10 11:46:49 +0200

    Creates Form
    Co-authored-by: r0binsahin <robin.sahin88@gmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import { Form } from "./components/form/Form";
 import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Form />
     </>
   );
 }
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,1 +1,12 @@
 import "./form.scss";
+
+export const Form = () => {
+  return (
+    <form action="submit" className="inputForm">
+      <input placeholder="Type something" type="text" className="searchInput" />
+      <button type="submit" className="searchButton">
+        Search
+      </button>
+    </form>
+  );
+};
