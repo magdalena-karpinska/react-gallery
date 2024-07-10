8774421f8ebec34ea30732231bf65d91a580cb0c
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-07-10 14:29:01 +0200

    changed fetch to be async await
    Co-authored-by: Kenny Dinh <kenny.t.dinh93@gmail.com>
    Co-authored-by: Robin Sahin <robin.sahin88@gmail.com>
    Co-authored-by: Magdalena Karpinska <magdalena.karpinska13@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,12 @@ import { Navbar } from "./components/navbar/Navbar";
 function App() {
   const [searchWord, setSearchWord] = useState("");
 
-  const getData = (searchWord: string) => {
-    // const res = fetch(`https://omdbapi.com?s=${searchWord}&apikey=515662d0`)
-    const res = fetch(
-      `https://api.unsplash.com/search/photos?query=${searchWord}&client_id=qRrrr0-zbjloU_Vk_hfaXzP-xS2FwQ8q9qeJBZtBdVY
-`
-    )
-      .then((data) => data.json())
-      .then((data) => console.log("data: ", data));
-    console.log("res: ", res);
-    console.log("word: ", searchWord);
+  const getData = async (searchWord: string) => {
+    const res = await fetch(
+      `https://api.unsplash.com/search/photos?query=${searchWord}&client_id=qRrrr0-zbjloU_Vk_hfaXzP-xS2FwQ8q9qeJBZtBdVY`
+    );
+    const data = await res.json();
+    console.log(data);
   };
 
   const handleSubmit = (e: FormEvent) => {
