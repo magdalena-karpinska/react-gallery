cc354fd501c689df2bc44a61168a73b323f282ae
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-07-10 11:59:54 +0200

    Initiates an Input form
    Co-authored-by: r0binsahin <robin.sahin88@gmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./form.scss";
 
 export const Form = () => {
+  const [searchWord, setSearchWord] = useState("");
+
+  const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+  };
   return (
     <form action="submit" className="inputForm">
-      <input placeholder="Type something" type="text" className="searchInput" />
+      <input
+        value={searchWord}
+        onChange={(e) => setSearchWord(e.target.value)}
+        placeholder="Type something"
+        type="text"
+        className="searchInput"
+      />
       <button type="submit" className="searchButton">
         Search
       </button>
