662ad7a7e30572a4ce2bea12ba86dce327d1f356
r0binsahin <robin.sahin88@gmail.com>
2024-07-11 11:55:17 +0200

    tanstack router implemented
    Co-authored-by: magdalena-karpinska <magdalena.karpinska13@gmail.com>
    Co-authored-by: JonathanZeray <jonathan.zeray@hotmail.com>
    Co-authored-by: iiiamken <kenny.t.dinh93@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,17 @@
-import "./App.scss";
-import { FormEvent, useState } from "react";
-import { Form } from "./components/form/Form";
-import { Navbar } from "./components/navbar/Navbar";
-import { getImages } from "./services/getImages";
-import { Image } from "./models/Image";
-import { DisplayImages } from "./components/displayImages/DisplayImages";
+import './App.scss';
+
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 function App() {
-  const [searchWord, setSearchWord] = useState("");
-  const [imageData, setImageData] = useState<Image[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    const data = await getImages(searchWord, 1);
-    setImageData(data);
-    setCurrentPage(1);
-  };
-
-  const handlePageChange = async (pageNumber: number) => {
-    const data = await getImages(searchWord, pageNumber);
-    setImageData(data);
-    setCurrentPage(pageNumber);
-  };
-
-  return (
-    <>
-      <Navbar />
-      <Form
-        handleSubmit={(e) => handleSubmit(e)}
-        setSearchWord={setSearchWord}
-        searchWord={searchWord}
-      />
-      <DisplayImages images={imageData} />
-      <div>
-        {currentPage > 1 && (
-          <button onClick={() => handlePageChange(currentPage - 1)}>
-            Previous
-          </button>
-        )}
-
-        <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
-      </div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/routes/__root.tsx
@@ -0,0 +1,11 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { Navbar } from '../components/navbar/Navbar';
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Navbar />
+      <Outlet />,
+    </>
+  ),
+});
